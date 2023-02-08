eba6e822f4ee4e46ab9c4695c66bd28d1283d373
ceshmina <shu@ceshmina.com>
2023-02-08 18:24:19 +0900

    タグ情報を参照してライブラリ整理

--- README.md
+++ README.md
@@ -1,16 +1,39 @@
-# walkman-util
+# mac-music-util
 
-Macでウォークマン管理するためのコード群です。(個人用)
+Macで音楽を管理するためのコード群です。(個人用)
 
-## setup
+## タグ情報を元にライブラリ整理
 
-ウォークマンを開発者モードにして、USBデバッグを有効化しておきます。また、Macにadbコマンドが必要です
+docker-composeが必要です。
+リポジトリ直下に `.env` を作成し、Macのミュージックライブラリのパスを指定します (以下は例)
+
+```
+MUSIC_LIB='/Users/shu/Music/XLD'
+```
+
+音源のタグデータを参照して、ファイルパスを `{アルバムアーティスト}/{アルバム}/{トラック番号}_{曲名}.flac` にリネームします。
+該当のタグ情報がない部分はスキップし、存在するタグのみ参照して置き換えます。
+
+```sh
+docker-compose up python
+```
+
+現状FLACのみの対応です。以下の文字はMacまたはAndroidで使用不可、または扱いが面倒なため、 `_` に置換されます。
+
+- `/`
+- 半角スペース
+- `:`
+- `?`
+
+## Androidデバイスに音楽を同期
+
+Androidデバイスを開発者モードにして、USBデバッグを有効化しておきます。また、Macにadbコマンドが必要です
 
 ```sh
 brew install android-platform-tools
 ```
 
-リポジトリ直下に `settings.ini` を作成し、Mac/ウォークマンそれぞれのミュージックライブラリのパスを指定します (以下は例)
+リポジトリ直下に `settings.ini` を作成し、Mac/Androidそれぞれのミュージックライブラリのパスを指定します (以下は例)
 
 ```python
 [DEFAULT]
@@ -18,9 +41,7 @@ mac_music_path = '/Users/shu/Music/XLD'
 walkman_music_path = '/sdcard/Music'
 ```
 
-## sync
-
-現状ではMac側に存在するすべての音源をウォークマンにコピーするため時間がかかります。ファイル名が一致した場合上書きされます。
+現状ではMac側に存在するすべての音源をAndroidにコピーするため時間がかかります。ファイル名が一致した場合上書きされます。
 
 ```sh
 python3 sync.py
--- python/main.py
+++ python/main.py
@@ -1,8 +1,49 @@
-import mutagen
+import glob
+from mutagen.flac import FLAC
+import os
+import shutil
 
 
 def main():
-    print('Hello poetry!')
+    flac_paths = glob.glob(f'/music/**/*.flac', recursive=True)
+    trans_map = str.maketrans({
+        '/': '_',
+        ' ': '_',
+        ':': '_',
+        '?': '_'
+    })
+
+    for path in flac_paths:
+        artist, album, file = path.split('/')[2:]
+
+        audio = FLAC(path)
+        change = False
+
+        tag_artist = audio.tags.get('albumartist', [''])[0].translate(trans_map)
+        if tag_artist != '' and artist != tag_artist:
+            artist = tag_artist
+            change = True
+
+        tag_album = audio.tags.get('album', [''])[0].translate(trans_map)
+        if tag_album != '' and album != tag_album:
+            album = tag_album
+            change = True
+        
+        # TODO: トラック番号がintにキャストできない場合、エラーになる
+        tag_track = int(audio.tags.get('tracknumber', [''])[0])
+        tag_title = audio.tags.get('title', [''])[0].translate(trans_map)
+        tag_file = f'{tag_track:02}_{tag_title}.flac'
+        if tag_file != '' and file != tag_file:
+            file = tag_file
+            change = True
+        
+        if change:
+            target_dir = f'/music/{artist}/{album}'
+            target_path = f'{target_dir}/{file}'
+            
+            print(f'rename {path} -> {target_path}')
+            os.makedirs(target_dir, exist_ok=True)
+            shutil.move(path, target_path)
 
 
 if __name__ == '__main__':
